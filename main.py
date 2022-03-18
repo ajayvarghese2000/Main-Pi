d434480c38019464cbe57636c6741a0fb242d958
Adi-Kashyap <adi1.kashyap@gmail.com>
2022-03-18 00:40:55 +0000

    Added Code to Check for the CONNECTION_TOGGLE_SWITCH

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ import socketio             # The socketIO server that will wait for images from
 import uvicorn              # The ASGI that will run the service
 from drone import drone     # The drone module
 from time import sleep      # Used for Simple delays
+import RPi.GPIO as GPIO     # Used to Access RPi GPIO Pins
+
+# Setting the Pin naming method
+GPIO.setmode(GPIO.BCM)
 
 # Creating the sockets server
 sio = socketio.AsyncServer(async_mode="asgi")
@@ -13,14 +17,24 @@ DNAME = '10'
 URL = "https://ajayvarghese.me"
 Drone = drone(DNAME, URL)
 
-# Checks if the drone should be sending data or not
-########## REPLACE WITH TOGGLE SWITCH CODE #################
-CONNECTION_SWITCH = True
+# Pin the Connection Toggle is connected to
+CONNECTION_TOGGLE_PIN = 23
+
+# Mapping up the connection toggle to the pin its connected to
+GPIO.setup(CONNECTION_TOGGLE_PIN, GPIO.IN)
+
+# Function to check if the drone should be sending data or not
+# Returns True or False
+def CONNECTION_SWITCH(): 
+    if GPIO.input(CONNECTION_TOGGLE_PIN):
+        return True
+    
+    return False
 
 # Connected Status
 CONNECTED = False
 
-if (CONNECTION_SWITCH == True):
+if (CONNECTION_SWITCH() == True):
     # Connect the drone to the server list
     while(CONNECTED == False):
         CONNECTED = True
@@ -35,10 +49,10 @@ if (CONNECTION_SWITCH == True):
 async def getdata(sid, mes):
 
     # Using the gloabl variables 
-    global CONNECTED, CONNECTION_SWITCH
+    global CONNECTED
 
     # Checks if we're meant to send data
-    if (CONNECTION_SWITCH == True):
+    if (CONNECTION_SWITCH() == True):
 
         # Checks if the drone is connected
         if(CONNECTED == True):
