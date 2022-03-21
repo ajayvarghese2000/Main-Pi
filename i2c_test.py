215f477659c4b0b4fe8ab09791121969fc5f79e4
ajayvarghese2000 <ajayvarghese2000@gmail.com>
2022-03-21 16:28:21 +0000

    updated the testing i2c code

--- i2c_test.py
+++ i2c_test.py
@@ -1,20 +1,34 @@
-from smbus import SMBus
-from time import sleep
+## [Imports]
+from smbus2 import SMBus    # Used to communicate with the i2c bus on pi
 
-bus = SMBus(1)
-address = 0x4c
+# Initialising the I2C Bus
+BUS = SMBus(1)
 
-def read_test():
+# Address of the i2c device
+ADDRESS = 0x4d
+
+# The Number of Blocks of 8 bits to read from the device
+BLOCKSIZE = 2   # [WARNING] If set incorrectly the i2c bus will block out
+
+# Simple Function to read from a registar and combine two blocks of data and retrun as a HEX
+def read_test(registar_address):
+    # Use the Global Variables
+    global BLOCKSIZE, ADDRESS, BUS
+
+    # Attempt read from bus
     try:
-        dat = bus.read_byte_data(address, 1)
-        return dat
+        # Attempt read from device
+        dat = BUS.read_i2c_block_data(ADDRESS, registar_address, BLOCKSIZE)
+
+        # Combine the Values
+        combine = dat[1] << 8 | dat[0]
+
+        # Retrun the Hex
+        return hex(combine)
+    # Returns 1 on failure
     except:
-        return "error"
-    
+        return 1
 
-
-while True:
-    data_recieved = read_test()
-    print(data_recieved)
-    sleep(1)
+# Try reading from registar 1
+print(read_test(0))
 
